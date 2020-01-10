f3849eb2fc60f6254a22b7ef369eabee4355795e
ac565708 <ac565708@chichester.esirem-ad.ad.u-bourgogne.fr>
2020-01-10 15:30:36 +0100

    premiers commentaires

--- src/main/java/enigma/Rotor.java
+++ src/main/java/enigma/Rotor.java
@@ -8,41 +8,41 @@ public class Rotor {
     private int notch1 = -1;
     private int notch2 = -1;
 
-    public int getPosition() {
-        return position;
+    public int getPosition() {                                                  //cette fonction récupère la position du rotor
+        return position;                                                        //puis la renvoie
     }
 
-    public void setPosition(int posn) {
+    public void setPosition(int posn) {                                         //cette position définie la position du rotor
         position = posn;
     }
     
-	public static Rotor rotorFactory(String str, String notches){
-		char[] s = str.trim().replace(" ", "").toCharArray();
-		int[] cipher = new int[26];
-		for (int i = 0; i< 26; i++){
-			cipher[i] = toIndex(s[i]);
-		}
-		s = notches.trim().replace(" and ", "").toCharArray();
-		if (s.length == 2){
-			return new Rotor(cipher, toIndex(s[0]), toIndex(s[1]));
-		} else {
-			return new Rotor(cipher, toIndex(s[0]));
-		}
+    public static Rotor rotorFactory(String str, String notches){               //
+	char[] s = str.trim().replace(" ", "").toCharArray();                   //
+	int[] cipher = new int[26];                                             //
+	for (int i = 0; i< 26; i++){                                            //
+		cipher[i] = toIndex(s[i]);                                      //
+	}
+	s = notches.trim().replace(" and ", "").toCharArray();                  //
+	if (s.length == 2){                                                     //
+		return new Rotor(cipher, toIndex(s[0]), toIndex(s[1]));         //
+	} else {                                                                //
+		return new Rotor(cipher, toIndex(s[0]));                        //
+	}
 		
-	}
+    }
 	
-	Rotor(int[] c, int notch1, int notch2) {
-		this.notch1 = notch1;
-		this.notch2 = notch2;
-		cipher = c;
-		createBCipher();
-	}
+    Rotor(int[] c, int notch1, int notch2) {
+            this.notch1 = notch1;
+            this.notch2 = notch2;
+            cipher = c;
+            createBCipher();
+    }
 	
-	Rotor(int[] c, int notch1) {
-		this.notch1 = notch1;
-		cipher = c;
-		createBCipher();
-	}
+    Rotor(int[] c, int notch1) {
+            this.notch1 = notch1;
+            cipher = c;
+            createBCipher();
+    }
 
     public int convertForward(int p) {
         return ((cipher[((p+position)%26+26)%26]-position)%26+26)%26;
